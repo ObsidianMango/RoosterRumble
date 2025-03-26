53fa5985a247cca2f3e3e9f8b5929e7aaddd08d2
ObsidianMango <steve.ragucci@gmail.com>
2025-03-25 20:41:08 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,17 +1,19 @@
-self.addEventListener('install', (event) => {
+self.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open('v1').then((cache) => {
+    caches.open('v1').then(function(cache) {
       return cache.addAll([
         './index.html',
         './manifest.json',
-
+        // Include additional assets (CSS, images, icons) as needed
       ]);
     })
   );
 });
 
-self.addEventListener('fetch', (event) => {
+self.addEventListener('fetch', function(event) {
   event.respondWith(
-    caches.match(event.request).then((response) => response || fetch(event.request))
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
   );
 });
